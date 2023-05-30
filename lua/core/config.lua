4277b4f3a4f1fa1bcd913ee1857cd4ac47fe0cf4
Avakumov Vladimir <avakdev@gmail.com>
2023-05-31 02:48:12 +0300

    modify config settings

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -1,2 +1,9 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+-- Indent Settings
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,60 +1,53 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = 
-{
-	{"phaazon/hop.nvim",
-	config = function()
-    	require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-	end
-	},
+local plugins = {
+	{ "phaazon/hop.nvim" },
 	{
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v2.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim", 
-	    "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
-            "s1n7ax/nvim-window-picker"
-        }
-    },
-    {"nvim-treesitter/nvim-treesitter"},
-    {"neovim/nvim-lspconfig"},
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+			"s1n7ax/nvim-window-picker",
+		},
+	},
+	{ "nvim-treesitter/nvim-treesitter" },
+	{ "neovim/nvim-lspconfig" },
 
-    { "ellisonleao/gruvbox.nvim", priority = 1000 },
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
 
-    {
-      "williamboman/mason.nvim",
-      build = ":MasonUpdate" -- :MasonUpdate updates registry contents
-    },
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+	},
 
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-buffer'}, 
-    {'hrsh7th/cmp-path'},
-    {'hrsh7th/cmp-cmdline'}, 
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp-signature-help'}, 
-    { 'hrsh7th/vim-vsnip' },
-    {'hrsh7th/vim-vsnip-integ'},
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+	{ "hrsh7th/vim-vsnip" },
+	{ "hrsh7th/vim-vsnip-integ" },
 
-
-   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {'jose-elias-alvarez/null-ls.nvim'}
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{ "jose-elias-alvarez/null-ls.nvim" },
 }
 
-
 require("lazy").setup(plugins)
-
