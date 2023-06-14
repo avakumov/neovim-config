0fec2cb72ed8919c0db57ed824138042f15853c0
Avakumov Vladimir <avakdev@gmail.com>
2023-06-14 08:54:13 +0300

    added plugin for support russian language

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -59,6 +59,15 @@ local plugins = {
 
 	-- terminals
 	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+	{
+		"Wansmer/langmapper.nvim",
+		lazy = false,
+		priority = 1, -- High priority is needed if you will use `autoremap()`
+		config = function()
+			require("langmapper").setup({--[[ your config ]]
+			})
+		end,
+	},
 }
 
 require("lazy").setup(plugins)
