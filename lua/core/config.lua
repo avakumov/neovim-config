d713ab47b25036ba30cde0359a2bfb7d339ea18b
Avakumov Vladimir <avakdev@gmail.com>
2023-07-07 15:53:45 +0300

    create blink cursor

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -9,6 +9,12 @@ vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 vim.opt.timeoutlen = 500
 
+-- Blink cursor
+vim.o.guicursor = table.concat({
+	"n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+	"i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+	"r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
+}, ",")
 -- Clipboard
 vim.opt.clipboard = "unnamedplus"
 
